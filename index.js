a0ffcf4ae5308beb99da923eba3d0d376a4379e2
patrick-replogle <patrickr1138@gmail.com>
2020-01-21 16:23:23 -0800

    test again

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const server = require("./api/server.js");
 
-const port = process.env.PORT || 4000;
-
+const port = process.env.PORT || 8080;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
