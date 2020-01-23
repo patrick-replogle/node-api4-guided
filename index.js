69c38b0cfb58d0c88ee0280a596b794dbfcd4496
patrick-replogle <patrickr1138@gmail.com>
2020-01-22 19:25:41 -0800

    testing again

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const server = require("./api/server.js");
 
 const host = process.env.HOST || "0.0.0.0";
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 4000;
 
 server.listen(port, host, () => {
   console.log(`Running at http://${host}:${port}`);
